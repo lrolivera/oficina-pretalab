b032b7026c3f2da65d449c48171e2e19ea030e2d
lrolivera <Larissinha.firmino@gmail.com>
2022-05-21 16:28:38 -0300

    Subindo a primeira parte do projeto do site

---
+++ primeiroCodigo.js
@@ -0,0 +1,6 @@
+const nomeUsuario = prompt("Me chamo Larissa, qual é o seu nome?");
+const sobrenomeUsuario = prompt("Qual é o seu sobrenome?");
+
+alert("Bem vinda ao meu site, " + nomeUsuario + " " + sobrenomeUsuario+ "!");
+
+
